ccaa48a35a0b2451c0c5e4c5c84b921e7a202d66
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-04-17 13:29:37 -0400

    feat(app.js): add Swagger middleware to serve API documentation feat(swagger.js): implement Swagger middleware to serve API documentation

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-// app.js
-
 import express from 'express';
 import userRoutes from './routes/users.js';
+import swaggerMiddleware from './swagger.js';
 
 const app = express();
 
+// Mount Swagger middleware on /api-docs route
+swaggerMiddleware(app);
+
+// Configure application to parse JSON data in request bodies
 app.use(express.json());
+
+// Mount userRoutes middleware on /api/users route
 app.use('/api/users', userRoutes);
 
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
