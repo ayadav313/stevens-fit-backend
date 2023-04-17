94050e13c36b64d60329bba7a6668046c7b1562e
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-04-17 13:31:39 -0400

    docs(exercises.js): add Swagger documentation for all routes in the Exercises API

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,9 +1,45 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: Exercises
+ *     description: API for managing exercises
+ */
+
 import express from 'express';
 import exerciseMethods from '../data/exercises.js';
 
 const router = express.Router();
 
-// POST /exercises
+/**
+ * @swagger
+ * /exercises:
+ *   post:
+ *     summary: Creates a new exercise
+ *     tags:
+ *       - Exercises
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               target:
+ *                 type: string
+ *               bodyPart:
+ *                 type: string
+ *               equipment:
+ *                 type: string
+ *               gifUrl:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad request
+ */
 router.post('/', async (req, res) => {
   try {
     const { name, target, bodyPart, equipment, gifUrl } = req.body;
@@ -14,7 +50,42 @@ router.post('/', async (req, res) => {
   }
 });
 
-// GET /exercises
+/**
+ * @swagger
+ * /exercises:
+ *   get:
+ *     summary: Returns a list of exercises
+ *     tags:
+ *       - Exercises 
+ *     responses:
+ *       200:
+ *         description: A list of exercises
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 612db8f48a7c18bf22004b0a
+ *                   name:
+ *                     type: string
+ *                     example: Squat
+ *                   target:
+ *                     type: string
+ *                     example: Legs
+ *                   bodyPart:
+ *                     type: string
+ *                     example: Quads
+ *                   equipment:
+ *                     type: string
+ *                     example: Barbell
+ *                   gifUrl:
+ *                     type: string
+ *                     example: https://example.com/squat.gif
+ */
 router.get('/', async (req, res) => {
     try {
       const exerciseList = await exerciseMethods.getAll();
@@ -25,7 +96,49 @@ router.get('/', async (req, res) => {
   });
   
 
-// GET /exercises/:id
+/**
+ * @swagger
+ * /exercises/{id}:
+ *   get:
+ *     summary: Get an exercise by ID
+ *     tags:
+ *       - Exercises
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the exercise
+ *     responses:
+ *       200:
+ *         description: An exercise object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   example: 612db8f48a7c18bf22004b0a
+ *                 name:
+ *                   type: string
+ *                   example: Squat
+ *                 target:
+ *                   type: string
+ *                   example: Legs
+ *                 bodyPart:
+ *                   type: string
+ *                   example: Quads
+ *                 equipment:
+ *                   type: string
+ *                   example: Barbell
+ *                 gifUrl:
+ *                   type: string
+ *                   example: https://example.com/squat.gif
+ *       404:
+ *         description: Exercise not found
+ */
 router.get('/:id', async (req, res) => {
   try {
     const exercise = await exerciseMethods.get(req.params.id);
@@ -35,7 +148,51 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// GET /exercises/body-part/:bodyPart
+/**
+ * @swagger
+ * /exercises/body-part/{bodyPart}:
+ *   get:
+ *     summary: Get a list of exercises by body part
+ *     tags:
+ *       - Exercises
+ *     parameters:
+ *       - in: path
+ *         name: bodyPart
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The body part for which to get exercises
+ *     responses:
+ *       200:
+ *         description: A list of exercises
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 612db8f48a7c18bf22004b0a
+ *                   name:
+ *                     type: string
+ *                     example: Squat
+ *                   target:
+ *                     type: string
+ *                     example: Legs
+ *                   bodyPart:
+ *                     type: string
+ *                     example: Quads
+ *                   equipment:
+ *                     type: string
+ *                     example: Barbell
+ *                   gifUrl:
+ *                     type: string
+ *                     example: https://example.com/squat.gif
+ *       404:
+ *         description: No exercises found
+ */
 router.get('/body-part/:bodyPart', async (req, res) => {
   try {
     const exerciseList = await exerciseMethods.getByBodyPart(req.params.bodyPart);
@@ -45,7 +202,51 @@ router.get('/body-part/:bodyPart', async (req, res) => {
   }
 });
 
-// GET /exercises/equipment/:equipment
+/**
+ * @swagger
+ * /exercises/equipment/{equipment}:
+ *   get:
+ *     summary: Get a list of exercises by equipment
+ *     tags:
+ *       - Exercises
+ *     parameters:
+ *       - in: path
+ *         name: equipment
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The equipment for which to get exercises
+ *     responses:
+ *       200:
+ *         description: A list of exercises
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 612db8f48a7c18bf22004b0a
+ *                   name:
+ *                     type: string
+ *                     example: Squat
+ *                   target:
+ *                     type: string
+ *                     example: Legs
+ *                   bodyPart:
+ *                     type: string
+ *                     example: Quads
+ *                   equipment:
+ *                     type: string
+ *                     example: Barbell
+ *                   gifUrl:
+ *                     type: string
+ *                     example: https://example.com/squat.gif
+ *       404:
+ *         description: No exercises found
+ */
 router.get('/equipment/:equipment', async (req, res) => {
   try {
     const exerciseList = await exerciseMethods.getByEquipment(req.params.equipment);
@@ -55,7 +256,51 @@ router.get('/equipment/:equipment', async (req, res) => {
   }
 });
 
-// GET /exercises/target/:target
+/**
+ * @swagger
+ * /exercises/target/{target}:
+ *   get:
+ *     summary: Get a list of exercises by target area
+ *     tags:
+ *       - Exercises
+ *     parameters:
+ *       - in: path
+ *         name: target
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The target area for which to get exercises
+ *     responses:
+ *       200:
+ *         description: A list of exercises
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 612db8f48a7c18bf22004b0a
+ *                   name:
+ *                     type: string
+ *                     example: Squat
+ *                   target:
+ *                     type: string
+ *                     example: Legs
+ *                   bodyPart:
+ *                     type: string
+ *                     example: Quads
+ *                   equipment:
+ *                     type: string
+ *                     example: Barbell
+ *                   gifUrl:
+ *                     type: string
+ *                     example: https://example.com/squat.gif
+ *       404:
+ *         description: No exercises found
+ */
 router.get('/target/:target', async (req, res) => {
   try {
     const exerciseList = await exerciseMethods.getByTarget(req.params.target);
